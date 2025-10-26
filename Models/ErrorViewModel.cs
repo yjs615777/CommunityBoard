47e7e200885d608b8faa0b3395e52520619d6cd5
JINSOO <JINSOO@DESKTOP-D8BOKQU>
2025-10-26 16:02:48 +0900

    초기 커밋.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CommunityBoard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
